34f2fc4dd656ceeb8a8c6077b5d140e7b037e67a
croakFrog <119742469+croakFrog@users.noreply.github.com>
2022-12-09 14:30:47 +0800

    cleaned up polybar folder; changed font for xterm

--- .config/polybar/scripts/launcher.sh
+++ .config/polybar/scripts/launcher.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-DIR="$HOME/.config/polybar/themes"
-
 launcher() {
-	rofi -no-config -no-lazy-grab -show drun -modi drun -theme "$DIR"/launcher.rasi
+	rofi -show drun
 }
 
 launcher
